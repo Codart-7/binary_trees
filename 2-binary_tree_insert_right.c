8bb664cf6695e178202bbb0bfef265d9f46c786c
Codart-7 <iconmoa@gmail.com>
2022-08-25 16:15:20 +0100

    [feature] inserts left and right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+  * binary_tree_insert_left - Insert a node on left of Binary tree
+  * @parent: node parent
+  * @value: key to insert in the new node
+  * Return: the address of the new node
+  */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = binary_tree_node(parent, value);
+	if (!new || !parent)
+		return (NULL);
+
+	if (parent->right)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
+
+	return (new);
+}
