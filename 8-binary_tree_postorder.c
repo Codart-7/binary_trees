8c32812feab4bc3f52b0a2a187f4900f016b2fdb
Michael Adisa <57605457+Codart-7@users.noreply.github.com>
2022-08-27 06:00:02 +0100

    Update 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -7,10 +7,14 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree && func)
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	else
 	{
 		binary_tree_postorder(tree->left, func);
 		binary_tree_postorder(tree->right, func);
-		func(tree->n);
 	}
+	func(tree->n);
 }
